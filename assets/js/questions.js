e0248af53c6807f4a7e10fc2d296126619ac7a8c
monysary <sary.mony@gmail.com>
2022-11-28 21:04:53 -0800

    Created functions to display first few options. Added eventlistener to select options

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,61 +1,64 @@
-choices = {
-    choice1: ["Light", "Dark"],
-    choice2: ["Boozy", "Refreshing"],
-    choice3: ["Sweet", "Complex"],
-    choice4: ["Vintage", "Modern"],
-    LBSV: ["Move to a new city every year", "Never leave your hometown"],
-    LBSM: ["In jail for a year", "Lose a year off your life"],
-    LBCV: ["Covered in scales", "Covered in fur"],
-    LBCM: ["World Cornhole champion", "World bowling champion"],
-    LRSV: ["Never feel cold again", "Never feel hot again"],
-    LRSM: ["Power of flight", "Power of invisibility"],
-    LRCV: ["Weigh five pounds", "Weigh 500 pounds"],
-    LRCM: ["People hear your thoughts", "Your life is livestreamed"],
-    DBSV: ["Average person now", "King 2,500 years ago"],
-    DBSM: ["Never pay for food", "Never pay for travel"],
-    DBCV: ["Wedding/tux every day", "Pajamas every day"],
-    DBCM: ["All birds try to attach you", "All dogs try to attack you"],
-    DRSV: ["Only RT movies with 94% or lower", "Only RT movies with 95% or higher"],
-    DRSM: ["Lose your other friends", "Lose you best friend"],
-    DRCV: ["No search engines", "No texting"],
-    DRCM: ["Live on the beach", "Live in a cabin"]
-}
+choices = [
+    ["Light", "Dark"],
+    ["Boozy", "Refreshing"],
+    ["Sweet", "Complex"],
+    ["Vintage", "Modern"],
+]
+
+questions = [
+    ["Move to a new city every year", "Never leave your hometown"], //1,2
+    ["In jail for a year", "Lose a year off your life"], //3,4
+    ["Covered in scales", "Covered in fur"], //5,6
+    ["World Cornhole champion", "World bowling champion"], //7,8
+    ["Never feel cold again", "Never feel hot again"], //9,10
+    ["Power of flight", "Power of invisibility"], //11,12
+    ["Weigh five pounds", "Weigh 500 pounds"], //13,14
+    ["People hear your thoughts", "Your life is livestreamed"], //15,16
+    ["Average person now", "King 2,500 years ago"], //17,18
+    ["Never pay for food", "Never pay for travel"], //19,20
+    ["Wedding/tux every day", "Pajamas every day"], //21,22
+    ["All birds try to attach you", "All dogs try to attack you"], //23,24
+    ["Only RT movies with 94% or lower", "Only RT movies with 95% or higher"], //25,26
+    ["Lose your other friends", "Lose you best friend"], //27,28
+    ["No search engines", "No texting"], //29,30
+    ["Live on the beach", "Live in a cabin"] //31,32
+]
 
 // Math: index = +1 or +2 -> *2-1 or *2 -> *2-1 or *2 -> *2-1 or *2 -> *2-1 or *2
 
 // Call drinks array with index-1
 
 drinksArr = [
-    "Espresso Martini",
-    "Grasshopper",
-    "Blue Motorcycle",
-    "Nuclear Reactor",
-    "Margarita",
-    "Negroni",
-    "Mai Tai",
-    "Pina Colada",
-    "Paloma",
-    "Cranberry Moscow Mule",
-    "Bee's Knees",
-    "Plum Soju",
-    "John Collins",
-    "Mojito",
-    "You Make Me Blush",
-    "The Leviathan",
-    "Old Fashioned",
-    "Planter's Punch",
-    "New Fashioned",
-    "Vegas Bomb",
-    "Manhattan",
-    "Black Russian",
-    "Jungle Bird",
-    "Old Cuban",
-    "Dark and Stormy",
-    "Mint Julep",
-    "Gold Rush",
-    "Flaming Volcano",
-    "Whiskey Sour",
-    "Painkiller",
-    "Lawn Flamingo",
-    "Peach Fuzz"
+    "Espresso Martini", //1
+    "Grasshopper", //2
+    "Blue Motorcycle", //3
+    "Nuclear Reactor", //4
+    "Margarita", //5
+    "Negroni", //6
+    "Mai Tai", //7
+    "Pina Colada", //8
+    "Paloma", //9
+    "Cranberry Moscow Mule", //10
+    "Bee's Knees", //11
+    "Plum Soju", //12
+    "John Collins", //13
+    "Mojito", //14
+    "You Make Me Blush", //15
+    "The Leviathan", //16
+    "Old Fashioned", //17
+    "Planter's Punch", //18
+    "New Fashioned", //19
+    "Vegas Bomb", //20
+    "Manhattan", //21
+    "Black Russian", //22
+    "Jungle Bird", //23
+    "Old Cuban", //24
+    "Dark and Stormy", //25
+    "Mint Julep", //26
+    "Gold Rush", //27
+    "Flaming Volcano", //28
+    "Whiskey Sour", //29
+    "Painkiller", //30
+    "Lawn Flamingo", //31
+    "Peach Fuzz" //32
 ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,49 @@
 // DOM variables
-var choose = document.querySelector("#questions");
-var options = document.querySelector("#options");
+var option1 = document.querySelector("#option1");
+var option2 = document.querySelector("#option2");
 
-// Display questions
+// Choice logic
+var choiceIndex = 0;
+var finalIndex = 0;
+
+// Display options function
+function displayChoice() {
+    option1.textContent = choices[choiceIndex][0];
+    option2.textContent = choices[choiceIndex][1];
+}
+displayChoice()
+
+// Display final questions function
+function displayQues() {
+    // 
+}
+
+// Event listener for choosing options
+option1.addEventListener("click", function() {
+    if (finalIndex == 0) {
+        finalIndex++;
+        choiceIndex++;
+        displayChoice();
+        console.log(finalIndex);
+    } else {
+        finalIndex *= 2;
+        finalIndex--;
+        choiceIndex++;
+        displayChoice();
+        console.log(finalIndex);
+    }
+})
+
+option2.addEventListener("click", function() {
+    if (finalIndex == 0) {
+        finalIndex += 2;
+        choiceIndex++;
+        displayChoice();
+        console.log(finalIndex);
+    } else {
+        finalIndex *= 2;
+        choiceIndex++;
+        displayChoice()
+        console.log(finalIndex);
+    }
+})
