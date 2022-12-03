3d83715b05bd98317e3650edc99442f1978ae445
monysary <sary.mony@gmail.com>
2022-12-03 14:07:50 -0800

    Added animations to options buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,37 @@
 // DOM variables
-const option1 = document.querySelector("#option1");
-const option2 = document.querySelector("#option2");
-const finalDrink = document.querySelector("#final-drink");
-const restartBtn = document.querySelector("#restart-btn");
-const title = document.querySelector("#title");
-const line = document.querySelector("#line");
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const textBtn1 = document.getElementById("textBtn1")
+const textBtn2 = document.getElementById("textBtn2")
+const finalDrink = document.getElementById("final-drink");
+const restartBtn = document.getElementById("restart-btn");
+const title = document.getElementById("title");
+const line = document.getElementById("line");
 
 // Index variables for pulling questions and drinks
 let choiceIndex = 0;
 let finalIndex = 0;
 
+// Button text animation variables
+const textAnimate = [
+    "text-animate-25",
+    "text-animate-50",
+    "text-animate-75",
+    "text-animate-100"
+]
+
+let textAnimateIndex = -1;
+
 // Display options function
-function displayChoice() {
-    option1.textContent = choices[choiceIndex][0];
-    option2.textContent = choices[choiceIndex][1];
+displayChoice = () => {
+    // option1.textContent = choices[choiceIndex][0];
+    // option2.textContent = choices[choiceIndex][1];
+    textBtn1.classList.add(textAnimate[textAnimateIndex]);
+    textBtn2.classList.add(textAnimate[textAnimateIndex]);
 }
 
 // Display final questions function
-function displayQues() {
+displayQues = () => {
     finalIndex--;
     option1.textContent = questions[finalIndex][0];
     option2.textContent = questions[finalIndex][1];
@@ -29,7 +43,7 @@ function displayQues() {
 }
 
 // Display drink
-function displayDrink() {
+displayDrink = () => {
     finalDrink.textContent = drinksArr[finalIndex];
     option1.setAttribute("class", "display-none");
     option2.setAttribute("class", "display-none");
@@ -37,19 +51,20 @@ function displayDrink() {
 }
 
 // Restart button function
-function restartAdv() {
+restartAdv = () => {
     choiceIndex = 0;
     finalIndex = 0;
     location.reload();
 }
 
 // Event listener for pressing restart button
-restartBtn.addEventListener("click", function() {
+restartBtn.addEventListener("click", () => {
     restartAdv()
 })
 
 // Event listener for choosing left option
-option1.addEventListener("click", function() {
+option1.addEventListener("click", () => {
+    textAnimateIndex +=1;
     if (choiceIndex === 0) {
         finalIndex++;
         choiceIndex++;
@@ -71,7 +86,8 @@ option1.addEventListener("click", function() {
 })
 
 // Event listener for choosing right option
-option2.addEventListener("click", function() {
+option2.addEventListener("click", () => {
+    textAnimateIndex +=1;
     if (choiceIndex === 0) {
         finalIndex += 2;
         choiceIndex++;
